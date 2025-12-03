e19ef1cfe6e73a33173924c1f74961b79c3171b6
Emilge11 <J389061@tafe.wa.edu.au>
2025-12-03 12:50:33 +0800

    Added a driver controller node, urdf and world path

--- launch/line_follower_launch.py
+++ launch/line_follower_launch.py
@@ -1,18 +1,26 @@
-import os
 import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
+from launch.substitutions import Command
 
 def generate_launch_description():
     package_dir = get_package_share_directory('line_follower')
-
-    world = os.path.join(package_dir, 'worlds', 'e-puck_botstudio_with_floor_sensors.wbt')
     params = os.path.join(package_dir, 'config', 'line_follower_params.yaml')
+    robot_description_path = os.path.join(package_dir, 'resource', 'my_robot.urdf')
 
-    webots = WebotsLauncher(world=world)
+    webots = WebotsLauncher(
+        world=os.path.join(package_dir, 'worlds', 'e-puck_botstudio_with_floor_sensors.wbt')
+    )
+
+    my_robot_driver = WebotsController(
+        robot_name='e-puck',
+        parameters=[
+            {'robot_description': robot_description_path},
+        ]
+    )
 
     line_follower_node = Node(
         package='line_follower',
@@ -23,6 +31,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         webots,
+        my_robot_driver,
         line_follower_node,
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
